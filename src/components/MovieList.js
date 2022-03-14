89f00272582145c46cdb9a03c6b453d3b2611318
DragosSHD <dragos.dragos.502@gmail.com>
2022-03-14 17:07:43 +0200

    Add history button on watchlist page.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -9,13 +9,19 @@ import {
     ListItem,
     Progress,
     Button,
-    Text, IconButton
+    Text, IconButton, HStack, Flex, Spacer, Divider, Icon, Tooltip
 } from "@chakra-ui/react";
+import {
+    CalendarIcon,
+    ViewIcon
+} from "@chakra-ui/icons";
 import Link from "next/link";
+import HistoryButton from "./HistoryButton";
+import {fetcher} from "../../utils/api";
 
 export default function MovieList(props) {
 
-    const {data, error} = useSWR(`/api/${props.apiRoute}`);
+    const {data, error, mutate} = useSWR(`/api/${props.apiRoute}`);
 
     if (!data) {
         return <Progress size="xs" isIndeterminate />;
@@ -37,22 +43,51 @@ export default function MovieList(props) {
         )
     }
 
+
     return (
         <Box>
             <Heading as='h2'>Your movie {props.listName}:</Heading>
-            <Center>
+            <Center
+                pt={['4', undefined, '10']}
+            >
                 <List bg='black'
                       w={{ base: '100%', md: '75%'}}
                       p={{base: '0 10px', md: '0 20px'}}
                       borderRadius={10}
                       className={'movies-list'}>
                     {data.map(({title, id}) => (
-                        <ListItem m='10px 0' className='movie-list-item' key={id}>
-                            <Link href={`/movies/${id}`} >
-                                <Center>
+                        <ListItem p='10px 0' className='movie-list-item' key={id}>
+                            <Flex align='center'>
+                                <Center maxW='200'>
                                     <Text as="span" fontSize='xl' padding='12px' isTruncated>{title}</Text>
                                 </Center>
-                            </Link>
+                                <Spacer/>
+                                <HStack>
+                                    <Link href={`/movies/${id}`} passHref>
+
+                                            <IconButton aria-label={'viewMovie'}>
+                                                <ViewIcon />
+                                            </IconButton>
+                                    </Link>
+                                    {props.addToHistoryBtn &&
+
+                                        <Tooltip label='Add to history'>
+                                            <IconButton
+                                                colorScheme='blue'
+                                                aria-label={'addToHistory'}
+                                                onClick={() => {
+                                                    mutate(() => {
+                                                        fetcher(`/api/watchlist/${id}`, {method: 'DELETE'});
+                                                        fetcher(`/api/history/${id}`, {method: 'PUT'});
+                                                    })
+                                                }}>
+                                                <CalendarIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                    }
+                                </HStack>
+                            </Flex>
+                            <Divider />
                         </ListItem>
                     ))}
                 </List>
